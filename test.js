ee37dd8ac78fe02957e1f7b5fa93436c65e989d9
Anthony Good <anthony@quipper.com>
2018-01-29 23:19:26 +0000

    try debugging in different places

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const { execSync } = require('child_process')
 const debug = require('./index')
+const bugger = eval(debug)
 
 const foo = { a: 1 }
 const boo = { b: false }
 
-eval(debug)()
+bugger()
+
+const zoo = 9
+
+bugger()
+
+foo.a = 22
+
+bugger()
 
 console.log('fin')
